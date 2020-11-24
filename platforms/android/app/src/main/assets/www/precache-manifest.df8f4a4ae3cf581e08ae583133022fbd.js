83f59ef953a6e0e6938d3f469d0e49719ed9d301
Oluwafemi <odoyoakereleisrael@gmail.com>
2020-11-24 11:56:51 +0100

    first commit

---
+++ platforms/android/app/src/main/assets/www/precache-manifest.df8f4a4ae3cf581e08ae583133022fbd.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a65761f07481fb6e15a94fe606963c4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b7d4d8bf0e21aa8f9faa",
+    "url": "./static/css/2.3070af1e.chunk.css"
+  },
+  {
+    "revision": "a892068cfe30349cc854",
+    "url": "./static/css/main.a5c25d2c.chunk.css"
+  },
+  {
+    "revision": "b7d4d8bf0e21aa8f9faa",
+    "url": "./static/js/2.9016acea.chunk.js"
+  },
+  {
+    "revision": "efdbb15cff2ff2a2a8046596ca9b24d1",
+    "url": "./static/js/2.9016acea.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a892068cfe30349cc854",
+    "url": "./static/js/main.acd184e3.chunk.js"
+  },
+  {
+    "revision": "f283ba8c2dd259acf373",
+    "url": "./static/js/runtime-main.87e52de5.js"
+  },
+  {
+    "revision": "f8f7aacb1bc651dd74c1667f33e5e66e",
+    "url": "./static/media/add_payment_bg.f8f7aacb.svg"
+  },
+  {
+    "revision": "87e515fbcae28e4e39a3703bbf37fa1b",
+    "url": "./static/media/avatar.87e515fb.png"
+  },
+  {
+    "revision": "989dfe3989dea63d09da7060f328830e",
+    "url": "./static/media/back.989dfe39.svg"
+  },
+  {
+    "revision": "f0cd84c7f918636745c7471479c0709f",
+    "url": "./static/media/bike_man.f0cd84c7.svg"
+  },
+  {
+    "revision": "0a3f01f0a4eff7a8ca058614f735b718",
+    "url": "./static/media/clear.0a3f01f0.svg"
+  },
+  {
+    "revision": "c03860f18e4bb135ded27516d2899c8a",
+    "url": "./static/media/credit_card.c03860f1.svg"
+  },
+  {
+    "revision": "24c12fb3edde17d584926ed6b2b410e4",
+    "url": "./static/media/delete-button.24c12fb3.svg"
+  },
+  {
+    "revision": "98ca5330c066448d7d33a5066e174ba2",
+    "url": "./static/media/freebies_icon.98ca5330.svg"
+  },
+  {
+    "revision": "e4e21ac518fb5bcf9709039f47ce74da",
+    "url": "./static/media/home_icon.e4e21ac5.svg"
+  },
+  {
+    "revision": "85539fef45496fca218fe05f6fb48cc6",
+    "url": "./static/media/log-out.85539fef.svg"
+  },
+  {
+    "revision": "d6e9ed6deee1753250f920b96cfc70f9",
+    "url": "./static/media/logo_new.d6e9ed6d.svg"
+  },
+  {
+    "revision": "607edb5ad1f222eab8b2892fa1bcb1a3",
+    "url": "./static/media/mastercard.607edb5a.svg"
+  },
+  {
+    "revision": "4db17acbf13f2fdea4a6253883bf3638",
+    "url": "./static/media/menu.4db17acb.svg"
+  },
+  {
+    "revision": "553b8aacddda2040c382064048e7a2e6",
+    "url": "./static/media/money.553b8aac.svg"
+  },
+  {
+    "revision": "b21e121e6e804143fc5893972f98097b",
+    "url": "./static/media/new_register_bg.b21e121e.svg"
+  },
+  {
+    "revision": "64822b247b9a57b66e7cd4948f7418da",
+    "url": "./static/media/order_history_icon.64822b24.svg"
+  },
+  {
+    "revision": "a76ceff17cb3e4d6854350d0df3f82e5",
+    "url": "./static/media/password_bg.a76ceff1.svg"
+  },
+  {
+    "revision": "bd839fcc9a2733abb71a6d7e83ba71bd",
+    "url": "./static/media/preview.bd839fcc.png"
+  },
+  {
+    "revision": "8dc1e6b0952c9639df00da8fbd67b013",
+    "url": "./static/media/profile.8dc1e6b0.svg"
+  },
+  {
+    "revision": "8b46f771896ac16d17f403ac95b5da3e",
+    "url": "./static/media/sargas_bg.8b46f771.svg"
+  },
+  {
+    "revision": "0fa96b0af528d8a9e903c118efb509d6",
+    "url": "./static/media/side_menu_bg.0fa96b0a.svg"
+  },
+  {
+    "revision": "4b24967418ef119bdfe5ec6c9af28dee",
+    "url": "./static/media/success.4b249674.svg"
+  },
+  {
+    "revision": "47a80d338fe6561de4d3b861ccda6135",
+    "url": "./static/media/timer_icon.47a80d33.svg"
+  },
+  {
+    "revision": "a3646e65b9a85cb1917fd00147978d77",
+    "url": "./static/media/token_page.a3646e65.svg"
+  },
+  {
+    "revision": "5d562614cca778036f808bc33af7ee71",
+    "url": "./static/media/track.5d562614.png"
+  }
+]);
